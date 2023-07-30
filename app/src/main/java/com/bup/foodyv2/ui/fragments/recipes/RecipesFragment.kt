39914baa678ad080523ace49710e4a977e852c9a
samia-bup <say2samirabup@gmail.com>
2023-07-29 22:57:54 -0500

    generate model classes for API

--- app/src/main/java/com/bup/foodyv2/ui/fragments/recipes/RecipesFragment.kt
+++ app/src/main/java/com/bup/foodyv2/ui/fragments/recipes/RecipesFragment.kt
@@ -1,20 +1,23 @@
-package com.bup.foodyv2
+package com.bup.foodyv2.ui.fragments.recipes
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bup.foodyv2.R
 
-
-class FavoriteRecipesFragment : Fragment() {
+class RecipesFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorite_recipes, container, false)
+        val view = inflater.inflate(R.layout.fragment_recipes, container, false)
+
+        return view
     }
+
 
 }
