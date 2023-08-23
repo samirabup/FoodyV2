5f1b4f1319b1e723562e4d39106be3a9b713888c
samia-bup <say2samirabup@gmail.com>
2023-08-23 09:39:16 -0500

    Contextual action mode done.

--- app/src/main/java/com/bup/foodyv2/ui/fragments/favorite/FavoriteRecipesFragment.kt
+++ app/src/main/java/com/bup/foodyv2/ui/fragments/favorite/FavoriteRecipesFragment.kt
@@ -3,14 +3,19 @@ package com.bup.foodyv2.ui.fragments.favorite
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.bup.foodyv2.R
 import com.bup.foodyv2.adapters.FavoriteRecipesAdapter
 import com.bup.foodyv2.databinding.FragmentFavoriteRecipesBinding
 import com.bup.foodyv2.viewmodels.MainViewModel
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -18,9 +23,8 @@ class FavoriteRecipesFragment : Fragment() {
 
     private var _binding : FragmentFavoriteRecipesBinding? = null
     private val binding get() = _binding!!
-
-    private val mAdapter: FavoriteRecipesAdapter by lazy { FavoriteRecipesAdapter() }
     private val mainViewModel: MainViewModel by viewModels()
+    private val mAdapter: FavoriteRecipesAdapter by lazy { FavoriteRecipesAdapter(requireActivity(), mainViewModel) }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -32,18 +36,42 @@ class FavoriteRecipesFragment : Fragment() {
         binding.mainViewModel = mainViewModel
         binding.mAdapter = mAdapter
 
+        setHasOptionsMenu(true)
         setupRecyclerView(binding.favoriteRecipesRecyclerView)
 
         return binding.root
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.favorite_recipes_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.deleteAll_favorite_recipes_menu) {
+            mainViewModel.deleteAllFavoriteRecipes()
+            showSnakeBar("All Recipes deleted.")
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 
     private fun setupRecyclerView(recyclerView: RecyclerView) {
         recyclerView.adapter = mAdapter
         recyclerView.layoutManager = LinearLayoutManager(requireContext())
     }
 
+    private fun showSnakeBar(message:String) {
+        Snackbar.make(
+            binding.root,
+            message,
+            Snackbar.LENGTH_SHORT
+        ).setAction("Okay"){}
+            .show()
+    }
+
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+        mAdapter.clearContextualActionMode()
     }
 }
